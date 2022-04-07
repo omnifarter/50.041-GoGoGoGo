5364286e7c650d0c929c3fc5f29bf0d93d0d07ed
Cao Bing Quan <caobingquan@hotmail.com>
2022-04-07 16:27:58 +0800

    fixed

--- ui/src/screen/Test/index.jsx
+++ ui/src/screen/Test/index.jsx
@@ -1,13 +1,36 @@
-import { Button, Form, Col } from "react-bootstrap";
+import { Button, Form, Col, Row, Container } from "react-bootstrap";
 import { useEffect, useState } from "react";
 
 function Test() {
     const [noOfNodes, setNoOfNodes] = useState();
-
-    const addBook = () => { };
-    const removeBook = () => { };
     const addNode = () => { };
     const removeNode = () => { };
+
+    const [bookTitleAdd, setBookTitleAdd] = useState('');
+    const [bookURLAdd, setBookURLAdd] = useState('');
+
+    const bookTitleHandler = (e) => {
+        setBookTitleAdd(e.target.value);
+    };
+    const bookURLHandler = (e) => {
+        setBookURLAdd(e.target.value);
+    };
+    const addBookHandler = (e) => {
+        e.preventDefault();
+        setBookTitleAdd('');
+        setBookURLAdd('');
+        console.log(bookTitleAdd+','+bookURLAdd);
+        return alert(bookTitleAdd + ',' + bookURLAdd)
+    }
+
+    const [bookIdRemove, setBookIdRemove] = useState();
+    const bookIdHandler = (e) => {
+        setBookIdRemove(e.target.value);
+    };
+    const removeBookHandler = (e) => {
+        e.preventDefault();
+        return alert("Book Id: " + bookIdRemove)
+    };
 
     return (
         <div>
@@ -26,38 +49,44 @@ function Test() {
                     Remove Node
                 </Button>{" "}
             </div>
-            <Col >
-                <div classname="App-header">
-                    <Form>
-                        <Form.Group className="mb-6">
-                            <Form.Label>Book Title</Form.Label>
-                            <Form.Control type="text" placeholder="Book Title" />
-                        </Form.Group>
-                        <Form.Group className="mb-6">
-                            <Form.Label>Book Image URL</Form.Label>
-                            <Form.Control type="text" placeholder="www.bookimageurl.com" />
-                        </Form.Group>
-                        <br />
-                        <Button variant="info" onClick={() => addBook()}>
-                            Add Book
-                        </Button>{" "}
-                    </Form>
-                </div>
-            </Col>
-            <Col>
-                <div classname="App-header">
-                    <Form>
-                        <Form.Group className="mb-6">
-                            <Form.Label>Book ID</Form.Label>
-                            <Form.Control type="text" placeholder="1" />
-                        </Form.Group>
-                        <br />
-                        <Button variant="info" onClick={() => removeBook()}>
-                            Remove Book
-                        </Button>{" "}
-                    </Form>
-                </div>
-            </Col>
+            <Container>
+                <Row>
+                    <Col>
+                        <h3 className="Library-title">Add Book!</h3>
+                        <div classname="App-header">
+                            <Form onSubmit={addBookHandler}>
+                                <Form.Group className="mb-6">
+                                    <Form.Label>Book Title</Form.Label>
+                                    <Form.Control type="text" placeholder="Book Title" value={bookTitleAdd} onChange={bookTitleHandler} required/>
+                                </Form.Group>
+                                <Form.Group className="mb-6">
+                                    <Form.Label>Book Image URL</Form.Label>
+                                    <Form.Control type="text" placeholder="www.bookimageurl.com" value={bookURLAdd} onChange={bookURLHandler} required/>
+                                </Form.Group>
+                                <br />
+                                <Button variant="info" type="submit">
+                                    Add Book
+                                </Button>{" "}
+                            </Form>
+                        </div>
+                    </Col>
+                    <Col>
+                        <h3 className="Library-title">Remove Book!</h3>
+                        <div>
+                            <Form onSubmit={removeBookHandler}>
+                                <Form.Group className="mb-6">
+                                    <Form.Label>Book ID</Form.Label>
+                                    <Form.Control type="number" placeholder="1"  value={bookIdRemove} onChange={bookIdHandler} required/>
+                                </Form.Group>
+                                <br />
+                                <Button variant="info" type="submit">
+                                    Remove Book
+                                </Button>{" "}
+                            </Form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
 
         </div>
     );
